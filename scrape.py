89f92dc04a8030cb55549f7d1bd14fe4a9f0e894
Marek Schir <schir2@gmail.com>
2020-04-20 12:54:09 -0400

    Writing initial functions.

--- scrape.py
+++ scrape.py
@@ -1,4 +1,19 @@
 from bs4 import BeautifulSoup
+import requests
 
-from settings import *
+from settings import BASE_URL
 
+
+def scrape(url: str):
+    # TODO Implement scrape function
+    return
+
+
+def get_next(page) -> str:
+    #  TODO Implement next function that finds the next page
+    return
+
+
+def write_data(data: dict) -> bool:
+    #  TODO Implement write_data function that writes data to csv line by line
+    return
