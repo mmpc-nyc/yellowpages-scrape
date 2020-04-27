37e2737c623fcb03dcd14432b01118e09269dbc0
Marek Schir <schir2@gmail.com>
2020-04-27 11:07:17 -0400

    Added a timing sleep function to scrape.py

--- yp_scrape/scripts/scrape.py
+++ yp_scrape/scripts/scrape.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 from slugify import slugify
+from time import sleep
 
 from settings import BASE_URL
 from settings import SEARCH_URL
@@ -33,12 +34,16 @@ def parse_listing(_listing):
         categories.append(Category(id=slugify(category.text),name=category.text))
 
     phones = _listing.find('div', {'class': 'phones'})
+    _parsed_listing['keywords'] = [Keyword(name=SEARCH_TERMS)]
     _parsed_listing['url'] = business_name['href'].split('?')[0]
     _parsed_listing['id'] = _parsed_listing['url'].split('-')[-1]
     _parsed_listing['categories'] = categories
     _parsed_listing['business_name'] = business_name.text
     _parsed_listing['address'] = street_address.text if street_address else ''
-    _parsed_listing['locality'] = locality.text if locality else ''
+    if not locality:
+        print(_listing)
+        return None
+    _parsed_listing['locality'] = locality.text
     *city, _parsed_listing['state'], _parsed_listing['zipcode'] = _parsed_listing['locality'].split(' ')
     _parsed_listing['city'] = ' '.join(city).strip(',')
     _parsed_listing['zipcode'] = _parsed_listing['locality'].split()[-1] if locality else ''
@@ -67,20 +72,25 @@ def main():
         for listing in listings:
             parsed_listing = parse_listing(listing)
             merge_listing(parsed_listing)
-            break
+            print(parsed_listing['business_name'])
         page = get_next(page_soup)
         break
 
 
 if __name__ == '__main__':
     page = get_page(SEARCH_TERMS, GEO_LOCATION_TERMS)
-    page_soup = page_to_soup(page)
-
+    page_number = 1
+    listing_number = 1
     while page:
+        page_soup = page_to_soup(page)
+        print(f'Page {page_number}\t {page.url}')
         listings = get_listings(page_soup)
         for listing in listings:
             parsed_listing = parse_listing(listing)
-            merge_listing(parsed_listing)
-            break
+            if parsed_listing:
+                merge_listing(parsed_listing)
+                print(f"{listing_number}\t {parsed_listing['business_name']}")
+            listing_number += 1
+        sleep(1)
+        page_number += 1
         page = get_next(page_soup)
-        break
